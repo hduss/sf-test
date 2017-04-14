84939879f71c311edda9107868a7896be6acd15a
hduss <delmas.theo@laposte.net>
2017-04-14 14:06:55 +0200

    mise a jour  des  gestions de login

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -18,7 +18,7 @@
         // Dernier nom d'utilisateur saisi
         $lastUsername = $authenticationUtils->getLastUsername();
      
-        return $this->render('security/login.html.twig', array(
+        return $this->render('CustomerBundle:Security:login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
