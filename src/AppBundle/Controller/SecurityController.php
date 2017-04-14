11324b32926a1c997fdacd58dd16ede0f63df8e0
hduss <delmas.theo@laposte.net>
2017-04-14 14:04:46 +0200

    mise en place de la sécurité du site, security.yml, creation SecurityController et changement des routes dans routing.yml

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+ // src/AppBundle/Controller/SecurityController.php
+ namespace AppBundle\Controller;
+  
+ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ use Symfony\Component\HttpFoundation\Request;
+  
+ class SecurityController extends Controller
+ {
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+     
+        // @var AuthenticationException $error (Erreur d'authentification)
+        $error = $authenticationUtils->getLastAuthenticationError();
+     
+        // Dernier nom d'utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+     
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
